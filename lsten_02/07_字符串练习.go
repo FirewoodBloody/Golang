7d9d96720723e65bad7f0e31aeee3389e75d9fdd
aizhu.aizhu@outlook.com <aizhu@1219271>
2018-11-15 14:58:04 +0800

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: aizhu.aizhu@outlook.com <aizhu@1219271>
    
    Conflicts:
    	lsten_02/06_时间_日期.go

--- lsten_02/07_字符串练习.go
+++ lsten_02/07_字符串练习.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+func main() {
+	var str = "hello"
+	var bytes = []byte(str)
+
+	for i := 0; i < len(str)/2; i++ { //i < len(str) 循环次数,只循环一般
+		tmp := bytes[len(str)-i-1]
+		bytes[len(str)-i-1] = bytes[i]
+		bytes[i] = tmp
+	}
+
+	str = string(bytes)
+
+	fmt.Println(str)
+}
