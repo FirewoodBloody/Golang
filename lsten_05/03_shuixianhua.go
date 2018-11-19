a900ceb0499c41dcaebaa54a4eb6563b452fad84
aizhu.aizhu@outlook.com <aizhu@1219271>
2018-11-19 18:04:40 +0800

    xx

---
+++ lsten_05/03_shuixianhua.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+//水仙花数:水仙花数是指一个 3 位数，它的每个位上的数字的 3次幂之和等于它本身
+// （例如：1^3 + 5^3+ 3^3 = 153）。
+
+func example1() {
+	for i := 100; i < 1000; i++ {
+		//if is_shuixianhua(i) == true {
+		//	fmt.Printf("%d is 水仙花数 ", i)
+		//}
+
+	}
+}
+
+func is_shuixianhua(a[b] int) bool {
+
+}
+
+func main() {
+	example1()
+}
