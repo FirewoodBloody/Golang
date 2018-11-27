72daf08dc81948c30d8ad93b741312b3cd5774e5
aizhu.aizhu@outlook.com <aizhu@1219271>
2018-11-27 22:03:41 +0800

    h
    Signed-off-by: aizhu.aizhu@outlook.com <aizhu@1219271>

--- lsten_08/08_example.go
+++ lsten_08/08_example.go
@@ -27,16 +27,30 @@ func example() {
 }
 
 func generatePassword() string {
-	var password []byte = make([]byte, 0, lenght)
-	for i := 0; i < lenght; i++ {
-
+	var password []byte = make([]byte, lenght, lenght)
+	var sourceSet string
+	if charset == "num" {
+		sourceSet = NumSet
+	} else if charset == "char" {
+		sourceSet = CharSeeet
+	} else if charset == "mix" {
+		sourceSet = fmt.Sprintf("%s%s", NumSet, CharSeeet)
+	} else if charset == "advance" {
+		sourceSet = fmt.Sprintf("%s%s%s", NumSet, CharSeeet, SpecSet)
+	} else {
+		sourceSet = NumSet
 	}
+	for i := 0; i < lenght; i++ {
+		index := rand.Intn(len(sourceSet))
+		password[i] = sourceSet[index]
+	}
+	return string(password)
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	example()
-	fmt.Printf("lenght:%d  charset:%s", lenght, charset)
+	//fmt.Printf("lenght:%d  charset:%s", lenght, charset)
 	password := generatePassword()
-	fmt.Printf("password:%s", password)
+	fmt.Printf("%s", password)
 }
