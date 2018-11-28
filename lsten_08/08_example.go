e226ccacc0865413e4d9e6814a03def143f7ea92
aizhu.aizhu@outlook.com <aizhu@1219271>
2018-11-28 10:09:58 +0800

    Default Changelist

--- lsten_08/08_example.go
+++ lsten_08/08_example.go
@@ -19,7 +19,7 @@ const (
 )
 
 func example() {
-	flag.IntVar(&lenght, "1", 16, "-1 生成密码的长度")
+	flag.IntVar(&lenght, "l", 16, "-1 生成密码的长度")
 	flag.StringVar(&charset, "t", "num",
 		"-t 制定密码生成的字符集, num只是用数字[0-9], char只是用英文字母[a-zA-Z],"+
 			"mix 使用数字和字母, advance:使用数字字母以及特殊字符")
@@ -27,16 +27,30 @@ func example() {
 }
 
 func generatePassword() string {
-	var password []byte = make([]byte, 0, lenght)
-	for i := 0; i < lenght; i++ {
-
+	var password []byte = make([]byte, lenght, lenght)
+	var sourcrSet string
+	if charset == "num" {
+		sourcrSet = NumSet
+	} else if charset == "char" {
+		sourcrSet = CharSeeet
+	} else if charset == "mix" {
+		sourcrSet = fmt.Sprintf("%s%s", NumSet, CharSeeet)
+	} else if charset == "advance" {
+		sourcrSet = fmt.Sprintf("%s%s%s", NumSet, CharSeeet, SpecSet)
+	} else {
+		sourcrSet = NumSet
 	}
+	for i := 0; i < lenght; i++ {
+		index := rand.Intn(len(sourcrSet))
+		password[i] = sourcrSet[index]
+	}
+	return string(password)
 }
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	example()
-	fmt.Printf("lenght:%d  charset:%s", lenght, charset)
+	//fmt.Printf("lenght:%d  charset:%s", lenght, charset)
 	password := generatePassword()
-	fmt.Printf("password:%s", password)
+	fmt.Printf("%s", password)
 }
