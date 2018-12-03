0d8d571cb445ff31818cfc8ec986e2afd4d14e57
aizhu.aizhu@outlook.com <aizhu@1219271>
2018-12-03 12:14:40 +0800

    x

--- lsten_12/stude_xinxi/stude.go
+++ lsten_12/stude_xinxi/stude.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/fatih/color"
+	"os"
 )
 
 type Student struct {
-	Username string
-	Score    float64
-	Grade    string
-	Sex      int
+	Username string  `json:"username"`
+	Score    float64 `json:"score"`
+	Grade    string  `json:"grade"`
+	Sex      string  `json:"sex"`
 }
 
 var (
-	sex     string
+	sexs    string
 	Xxslice map[string]Student = make(map[string]Student, 1000000)
 )
 
@@ -36,17 +38,18 @@ func Scans(a string) {
 		username string
 		score    float64
 		grade    string
-		sex      int
+		sex      string
 		sum      Student
 	)
 	switch a {
 	case "s", "S":
 		var a string
 		fmt.Print("请输入您要查询的学生姓名,如需查询全部学生信息,请按l/L:")
-		fmt.Scanf(&a)
+		fmt.Scanf("%s\n", &a)
 		if a == "l" || a == "L" {
 			for _, value := range Xxslice {
-				fmt.Printf("%v\n", value)
+				date, _ := json.Marshal(value)
+				fmt.Printf("%v\n", date)
 			}
 		} else {
 			_, ok := Xxslice[a]
@@ -57,6 +60,7 @@ func Scans(a string) {
 				defer color.Unset()
 				fmt.Println("您查询的学生不存在!")
 			}
+			fmt.Println()
 		}
 	case "c", "C":
 		for i := 0; i < 4; i++ {
@@ -71,28 +75,49 @@ func Scans(a string) {
 				sum.Grade = grade
 			} else if i == 2 {
 				fmt.Printf("请输入学生性别:")
-				fmt.Scanf("%d\n", &sex)
+				fmt.Scanf("%s\n", &sex)
 				sum.Sex = sex
 			} else if i == 3 {
 				fmt.Printf("请输入学生分数:")
 				fmt.Scanf("%f\n", &score)
 				sum.Score = score
 			}
-
-			Xxslice[username] = sum
 		}
+		Xxslice[username] = sum
 
 	case "i", "I":
-	case "q", "Q":
+		var (
+			num     int
+			selects Student
+		)
+		for {
+			fmt.Print("请选择您要修改的学生姓名：")
+			fmt.Scanf("%v\n", &username)
+			fmt.Print("请选择您要修改的学生信息（1：班级  2：性别  3：分数  4：全部）：")
+			fmt.Scanf("%v\n", &num)
+			if num == 1 {
+				fmt.Printf("请输入%v的班级:", username)
+				fmt.Scanf("%s\n", &grade)
+				Xxslice[username] = Student{Grade: grade}
 
+			} else if num == 2 {
+
+			} else if num == 3 {
+
+			} else if num == 4 {
+
+			}
+		}
+	case "q", "Q":
+		os.Exit(0)
 	}
 
 }
 
 func main() {
 	for {
-		fmt.Scan(&sex)
 		Caidan()
-		Scans(sex)
+		fmt.Scan(&sexs)
+		Scans(sexs)
 	}
 }
