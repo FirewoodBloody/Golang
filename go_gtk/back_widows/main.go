9dfe1c3b218e8983900f14bc275239ed3e04fab1
aizhu.aizhu@outlook.com <aizhu@1219271>
2019-01-20 11:59:10 +0800

    x

---
+++ go_gtk/back_widows/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/mattn/go-gtk/gtk"
+	"os"
+)
+
+func main() {
+	gtk.Init(&os.Args) //环境初始化
+
+	windows := gtk.NewWindow(gtk.WINDOW_TOPLEVEL) //新建空白窗口
+	windows.SetPosition(gtk.WIN_POS_CENTER)       //设置窗口居中
+	windows.SetTitle("GO Gtk")                    //设置窗口标题
+	windows.SetSizeRequest(300, 200)              //设置窗口高度和宽度
+
+	windows.Show() //显示窗口
+
+	gtk.Main() //主事件循环，等待用户输入
+}
