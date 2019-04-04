9a42c8901a08beec02473c4639d5603342fe2ffa
aizhu.aizhu@outlook.com <aizhu@1219271>
2019-04-04 20:20:39 +0800

    上传资料
    Signed-off-by: aizhu.aizhu@outlook.com <aizhu@1219271>

--- ExpressAIP/db/main.go
+++ ExpressAIP/db/main.go
@@ -14,30 +14,21 @@ func init() {
 
 	orm.RegisterDriver("oci8", orm.DROracle) //注册驱动
 
-	orm.RegisterDataBase("default", "oci8", "system/aizhu1219271@127.0.0.1:1521/Sf") //连接oracle数据库
+	orm.RegisterDataBase("default", "oci8", "system/aizhu1219271@127.0.0.1:1521/SF") //连接oracle数据库
 }
 
 //查询数据库
 func main() {
 	//aa := "顺丰快递"
-	//selects := fmt.Sprintf("update api set  c = ('已签收')，d = to_date('2018-06-30 23:59:59','yyyy-mm-dd hh24:mi:ss') where b = 753951456852")
+	selects := fmt.Sprintf("SELECT D FroM api WHERE A = '顺丰快递' AND C = '拒收'")
 	o := orm.NewOrm()
-	//var maps []orm.Params
-	//num, err := o.Raw(selects).Values(&maps)
-	//if err == nil && num > 0 {
-	//	fmt.Println(maps[0]["B"], len(maps)) // slene
-	//
-	//}
-	a, err := o.Raw("update api set  c = '已签收' where b = 753951456852").Exec()
-	if err != nil {
-		fmt.Println(err)
-	}
+	var maps []orm.Params
+	num, err := o.Raw(selects).Values(&maps)
 
-	n, err := a.RowsAffected()
-	if err != nil {
-		fmt.Println(err)
-	}
+	if err == nil && num > 0 {
+		fmt.Println(len(maps), num)
+		fmt.Println(maps[0]["D"])
 
-	fmt.Println(n)
+	}
 
 }
